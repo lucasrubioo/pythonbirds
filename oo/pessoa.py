e01223fedf1cb2224e82ee1d07eec2101b81d524
lucas <lucassilvarubio99@gmail.com>
2023-10-03 20:35:33 -0300

    criado atributo de nome e idade a classe pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,11 @@
 class Pessoa:
-    pass
+    def __init__(self, nome = None, idade = None):
+        self.nome = nome
+        self.idade = idade
+
+    def cumprimentar(self):
+        return 'Ola'
+
+if __name__ == '__main__':
+    p = Pessoa('Lucas')
+    print(p.cumprimentar(), p.nome)
